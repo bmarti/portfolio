e90db9e76ee0f728fe80c5ac8e97c259eaadf5dc
Baptiste <31932714+bmarti@users.noreply.github.com>
2023-04-01 14:09:18 +0200

    Add navigation menu

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './nav.css'
+import {RxHome} from 'react-icons/rx'
+import {RiUser2Line} from 'react-icons/ri'
+import {AiOutlineProfile} from 'react-icons/ai'
+import {BsBook} from 'react-icons/bs'
+import {AiOutlineMessage} from 'react-icons/ai'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><RxHome/></a>
+      <a href="#about"><RiUser2Line/></a>
+      <a href="#experience"><AiOutlineProfile/></a>
+      <a href="#portfolio"><BsBook/></a>
+      <a href="#contact"><AiOutlineMessage/></a>
+    </nav>
   )
 }
 
