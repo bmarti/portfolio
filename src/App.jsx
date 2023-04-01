9c23690db4c5a3797ad0f1bc8a74d012cc0b56da
Baptiste <31932714+bmarti@users.noreply.github.com>
2023-04-01 14:15:57 +0200

    Rename recommendations + add sections

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
-import Recommendations from './components/recommendations/Recommendations'
+import Recommandations from './components/recommandations/Recommandations'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 
@@ -18,7 +18,7 @@ const App = () => {
       <Experience />
       <Services />
       <Portfolio />
-      <Recommendations />
+      {/* <Recommandations /> */}
       <Contact />
       <Footer />
     </>
---
+++ src/components/recommandations/Recommandations.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './recommandations.css'
+
+const Recommandations = () => {
+  return (
+    <section id='recommandations'>Recommandations</section>
+  )
+}
+
+export default Recommandations
