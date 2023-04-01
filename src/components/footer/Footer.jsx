0856d68b43d452c4b1025297d74657913eee2a45
Baptiste <31932714+bmarti@users.noreply.github.com>
2023-04-01 19:03:09 +0200

    Add footer menu and copyright

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,25 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Baptiste Martinez</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; 2023 Baptiste Martinez. Tous droits réservés.</small>
+      </div>
+    </footer>
   )
 }
 
