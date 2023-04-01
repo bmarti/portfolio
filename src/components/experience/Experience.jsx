f54bdefc878f1b070449661abe3e7c9af775c1fb
Baptiste <31932714+bmarti@users.noreply.github.com>
2023-04-01 17:34:56 +0200

    Experiences part

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,142 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>Les compétences que j'ai acquises</h5>
+      <h5>Initié {"<"} Bon niveau {"<"} Confirmé {"<"} Expert</h5>
+      <h2>Expérience professionnelle</h2>
+
+      <div className='container experience__container'>
+        <div className='experience__languages'>
+          <h3>Langages maîtrisés</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Java</h4>
+                <small className='text-light'>Confirmé</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>C++</h4>
+                <small className='text-light'>Confirmé</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>SQL</h4>
+                <small className='text-light'>Confirmé</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>C#</h4>
+                <small className='text-light'>Bon niveau</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Javascript</h4>
+                <small className='text-light'>Initié</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Initié</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* END OF LANGUAGES */}
+
+        <div className='experience__framework'>
+          <h3>Frameworks connus</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Maven</h4>
+                <small className='text-light'>Confirmé</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Maven</h4>
+                <small className='text-light'>Confirmé</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>PostgreSQL</h4>
+                <small className='text-light'>Bon niveau</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Git</h4>
+                <small className='text-light'>Confirmé</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>SVN</h4>
+                <small className='text-light'>Confirmé</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>NodeJS</h4>
+                <small className='text-light'>Initié</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Docker</h4>
+                <small className='text-light'>Initié</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Tomcat</h4>
+                <small className='text-light'>Confirmé</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Spring</h4>
+                <small className='text-light'>Bon niveau</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill className='experience__details-icon'/>
+              <div>
+                <h4>Cassandra</h4>
+                <small className='text-light'>Bon niveau</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* END OF FRAMEWORKS */}
+      </div>
+    </section>
   )
 }
 
